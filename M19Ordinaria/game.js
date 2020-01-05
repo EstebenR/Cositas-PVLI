8572dd59886f3bd14539b144bf716f75b055d990
Esteban <estrestrepo+github@gmail.com>
2020-01-05 19:54:29 +0100

    Juego

--- M19Ordinaria/game.js
+++ M19Ordinaria/game.js
@@ -20,7 +20,6 @@ export default class Game extends Phaser.Scene {
     let wallLeft = new Wall(this,0,this.game.config.height/2,50,this.game.config.height);
     let wallRight = new Wall(this,this.game.config.width,this.game.config.height/2,50,this.game.config.height);
 
-    this.uiText = this.add.text(25,25,"Quedan x colisiones y Z segundos").setFontSize(40).setColor('#000000');
 
     this.player = new Player(this,700,400,10);
 
@@ -33,13 +32,50 @@ export default class Game extends Phaser.Scene {
         this.ballCollided(collidedBall.gameObject);
       }
       },this);
+
+    //UI
+    this.collisions = 15;
+    this.timeToPlay = 30;//secs
+    this.gameRunning = true;
+
+    let string = "Quedan " + this.collisions + " colisiones y " + this.timeToPlay + " segundos";
+    this.uiText = this.add.text(25,25,string).setFontSize(40).setColor('#000000');
+
+    this.timer = this.time.addEvent({
+      delay: 1000,                // ms
+      callback: this.updateUI,
+      args: [true],
+      callbackScope: this,
+      loop: true
+  });
+
+    this.spacebar = this.input.keyboard.addKey('SPACE');
+  }
+
+  updateUI(secondPassed){
+    if(secondPassed)this.timeToPlay--;
+    let string = "Quedan " + this.collisions + " colisiones y " + this.timeToPlay + " segundos";   
+    this.uiText.setText(string)
   }
 
   update(time, delta) {    
+    if(this.timeToPlay == 0 || this.collisions == 0){
+      this.matter.world.pause();
+      if(this.timeToPlay == 0)
+        {this.add.text(500,400,"HAS PERDIDO").setColor('#FF0000').setFontSize(60);}
+      else
+        {this.add.text(500,400,"HAS GANADO").setColor('#00FF00').setFontSize(60);}
+      this.add.text(350,500,"Pulsa espacio para reiniciar").setColor('#000000').setFontSize(40);
+      this.gameRunning = false;
+    }
+
+    if(this.spacebar.isDown && !this.gameRunning)
+      this.scene.restart();
   }
 
   ballCollided(collidedBall){
-    console.log(collidedBall);
+    this.collisions--;
+    this.updateUI(false);
     if(collidedBall.divisions < 3){
       new Ball(this,collidedBall.x,collidedBall.y,collidedBall.radius,10,collidedBall.divisions+1);
       new Ball(this,collidedBall.x,collidedBall.y,collidedBall.radius,10,collidedBall.divisions+1);
