3273a732e12ac6635fad5887bcbf46ed765046da
Esteban <estrestrepo+github@gmail.com>
2020-01-05 17:16:05 +0100

    Escenario

--- M19Extraordinaria/game.js
+++ M19Extraordinaria/game.js
@@ -3,9 +3,18 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {  
+    this.load.image('platform','Platform.png');
+    this.load.image('star','Star.png');
+    this.load.spritesheet('player','Player-sheet.png',{frameWidth: 20, frameHeight: 32});
   }
 
   create() {
+    this.platforms = [];
+    this.platforms.push(this.matter.add.image(300,200,'platform'));
+    this.platforms.push(this.matter.add.image(1100,200,'platform'));
+    this.platforms.push(this.matter.add.image(300,600,'platform'));
+    this.platforms.push(this.matter.add.image(1100,600,'platform'));
+    this.platforms.push(this.matter.add.image(700,400,'platform'));
   }
 
   update(time, delta) {    
