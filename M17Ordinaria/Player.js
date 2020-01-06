79b46dc3ee9d15df190f1eac752cba318ca56afa
Esteban <estrestrepo+github@gmail.com>
2020-01-06 16:58:07 +0100

    Entrada y movmiento

--- M17Ordinaria/Player.js
+++ M17Ordinaria/Player.js
@@ -5,8 +5,25 @@ export default class Player extends Phaser.Physics.Matter.Sprite{
         this.setFixedRotation();
         this.setScale(3);
 
+        this.speed = speed;
         this.up = scene.input.keyboard.addKey(keys.up);
         this.left = scene.input.keyboard.addKey(keys.left);
         this.right = scene.input.keyboard.addKey(keys.right);
+    }
+
+    preUpdate(t, dt){
+        this.setAngle(0);
+        
+        if(this.left.isDown){
+            this.setVelocityX(-this.speed);
+        }else if(this.right.isDown){
+            this.setVelocityX(this.speed);
+        }else{
+            this.setVelocityX(0);
+        }
+
+        if(this.up.isDown){
+            this.setVelocityY(-this.speed*5);
+        }
     }
 }
